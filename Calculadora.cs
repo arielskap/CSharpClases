389fab308c064d75eb7ada395676c5d10b491324
Ariel Villarreal <arielskap@gmail.com>
2019-04-09 16:39:33 -0300

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Calculadora
+    {
+        private static string ValidarOperador(string operador)
+        {
+            if(operador=="+"||operador=="-"||operador=="/"||operador=="*")
+            else
+            {
+                operador = "+";
+            }
+            return operador;
+        }
+        public double Operar(Numero num1, Numero num2, string operador)
+        {
+            double resultado=0;
+            operador = ValidarOperador(operador);
+            switch(operador)
+            {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        resultado = num1 / num2;
+                    }
+                    else
+                    {
+                        resultado = 0;
+                    }
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Numero.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Numero
+    {
+        private double numero;
+        public Numero()
+        {
+            this.numero = 0;
+        }
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+        public Numero(string numero)
+        {
+            this.SetNumero(numero);
+        }
+        private static double ValidarNumero(string strNumero)
+        {
+            double numerico;
+            if(double.TryParse(strNumero,out numerico))
+            { }
+            else
+            {
+                numerico = 0;
+            }
+            return numerico;
+        }
+        private void SetNumero(string numero)
+        {
+            this.numero = ValidarNumero(numero);
+        }
+        public static string BinarioDecimal(string binario)
+        {
+            return binario;
+        }
+        public static string DecimalBinario(double numero)
+        {
+            string numeroString="";
+            return numeroString;
+        }
+        public static string DecimalBinario(string numero)
+        {
+            return numero;
+        }
+        public static double operator -(Numero n1,Numero n2)
+        {
+            double resultado;
+            resultado = n1.numero - n2.numero;
+            return resultado;
+        }
+        public static double operator *(Numero n1,Numero n2)
+        {
+            double resultado;
+            resultado = n1.numero * n2.numero;
+            return resultado;
+        }
+        public static double operator /(Numero n1, Numero n2)
+        {
+            double resultado;
+            resultado = n1.numero / n2.numero;
+            return resultado;
+        }
+        public static double operator +(Numero n1, Numero n2)
+        {
+            double resultado;
+            resultado = n1.numero + n2.numero;
+            return resultado;
+        }
+        //hacer sobrecargar de == y !=
+
+        /*public static Numero operator ==()
+        {
+
+        }*/
+    }
+}
